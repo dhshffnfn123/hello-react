adc77fb5b5f28bb98d2a88d66770750465d41ded
B H An <anbyunghoon0107@gamil.com>
2021-10-31 00:06:07 +0900

    DOM에 이름달기

--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -18,13 +18,15 @@ class ValidationSample extends Component {
         this.setState({
             clicked: true,
             validated: this.state.password === '0000'
-        })
+        });
+        this.input.focus();
     }
 
     render() {
         return (
             <div>
                 <input
+                    ref={(ref) => this.input=ref}
                     type="password"
                     value={this.state.password}
                     onChange={this.handleChange}
