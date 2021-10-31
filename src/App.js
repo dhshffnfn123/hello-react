6ccc9412ae9d192e9932c2f367024871f3df5d9a
B H An <anbyunghoon0107@gamil.com>
2021-10-31 23:12:53 +0900

    배열

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import { Component } from "react/cjs/react.production.min";
-import ScrollBox from "./ScrollBox";
+import React, {Component} from "react";
+import IterationSample from "./IterationSample";
 
 class App extends Component {
   render() {
 
   
     return ( 
-    <div>
-      <ScrollBox ref={(ref) => this.scrollBox=ref} />
-      <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
-    </div>
+      <IterationSample />
     );
   }
 }
--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,14 +1,9 @@
 import React from "react";
 
 const IterationSample = () => {
-    return (
-        <ul>
-            <li>눈사람</li>
-            <li>얼음</li>
-            <li>눈</li>
-            <li>바람</li>
-        </ul>
-    )
+    const names = ['눈사람', '얼음', '눈', '바람'];
+    const nameList = names.map(name => <li>{name}</li>);
+    return <ul>{nameList}</ul>
 }
 
 export default IterationSample;
