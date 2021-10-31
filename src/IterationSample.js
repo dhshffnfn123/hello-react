b7dc1cc7f3eb13b95656550e1a7362ac23a7aed7
B H An <anbyunghoon0107@gamil.com>
2021-11-01 00:14:02 +0900

    filter

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -11,12 +11,25 @@ const IterationSample = () => {
     const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
 
     const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId, //nextId 값을 id로 설정한다.
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    };
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
 
-    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const namesList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>);
     return (
         <>
             <input value={inputText} onChange={onChange} />
-            <button>추가</button>
+            <button onClick={onClick}>추가</button>
             <ul>{namesList}</ul>
         </>
     )
