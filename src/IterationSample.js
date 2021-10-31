658cf4c82bc8b3fedbb5928e895ed2b60ef113a3
B H An <anbyunghoon0107@gamil.com>
2021-10-31 17:46:48 +0900

    컴포넌트의 반복

---
+++ src/IterationSample.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const IterationSample = () => {
+    return (
+        <ul>
+            <li>눈사람</li>
+            <li>얼음</li>
+            <li>눈</li>
+            <li>바람</li>
+        </ul>
+    )
+}
+
+export default IterationSample;
